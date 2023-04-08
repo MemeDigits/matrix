a11b7621563e5dcb12e9a90860bda0cef70b2b66
MemeDigits <130191257+MemeDigits@users.noreply.github.com>
2023-04-07 22:30:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var digitCount = Math.floor(windowWidth / 20);
 for (var i = 0; i < digitCount; i++) {
   var digitElement = document.createElement('span');
   digitElement.classList.add('digit');
-
+  
   if (Math.random() < 0.5) {
     digitElement.innerText = '69';
     digitElement.classList.add('digit-69');
@@ -16,11 +16,35 @@ for (var i = 0; i < digitCount; i++) {
   }
 
   digitElement.style.left = (i * 20) + 'px';
-  digitElement.style.top = Math.floor(Math.random() * windowHeight) + 'px';
-  digitElement.style.animationDelay = (Math.random() * 6) + 's';
+  digitElement.style.top = -(Math.floor(Math.random() * 500) + 100) + 'px';
+  
+  var shadowCount = Math.floor(Math.random() * 4);
+  var shadowString = '';
 
-  var fallingSpeed = Math.floor(Math.random() * 10) + 1;
-  digitElement.style.animationDuration = (60 / fallingSpeed) + 's';
+  for (var j = 1; j <= shadowCount; j++) {
+    shadowString += j * 2 + 'px ' + j * 2 + 'px 4px #0F0';
+    if (j < shadowCount) {
+      shadowString += ', ';
+    }
+  }
 
+  digitElement.style.textShadow = shadowString;
+  
   digitLayer.appendChild(digitElement);
 }
+
+setInterval(function() {
+  var digitElements = document.querySelectorAll('.digit');
+  var windowHeight = window.innerHeight;
+  
+  for (var i = 0; i < digitElements.length; i++) {
+    var digitElement = digitElements[i];
+    var top = parseFloat(digitElement.style.top);
+    if (top >= windowHeight) {
+      top = -(Math.floor(Math.random() * 500) + 100);
+      digitElement.style.left = (Math.floor(Math.random() * windowWidth)) + 'px';
+    }
+    top += Math.floor(Math.random() * 10) + 5;
+    digitElement.style.top = top + 'px';
+  }
+}, 50);
